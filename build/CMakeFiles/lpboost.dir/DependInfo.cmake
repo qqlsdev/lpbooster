93cad6f16692e0224f36e9b5d85542d5c9ccdf57
qqlsdev <salvadorQQLS@gmail.com>
2026-06-15 19:05:03 +0000

    Update to v1.6.8

---
+++ build/CMakeFiles/lpboost.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/linux-user/Documents/Stack-Booster/logger/logger.cpp" "CMakeFiles/lpboost.dir/logger/logger.cpp.o" "gcc" "CMakeFiles/lpboost.dir/logger/logger.cpp.o.d"
+  "/home/linux-user/Documents/Stack-Booster/main.cpp" "CMakeFiles/lpboost.dir/main.cpp.o" "gcc" "CMakeFiles/lpboost.dir/main.cpp.o.d"
+  "" "lpboost" "gcc" "CMakeFiles/lpboost.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/_deps/inih-subbuild/CMakeFiles/inih-populate.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inih-populate"
+  "CMakeFiles/inih-populate-complete"
+  "inih-populate-prefix/src/inih-populate-stamp/inih-populate-build"
+  "inih-populate-prefix/src/inih-populate-stamp/inih-populate-configure"
+  "inih-populate-prefix/src/inih-populate-stamp/inih-populate-download"
+  "inih-populate-prefix/src/inih-populate-stamp/inih-populate-install"
+  "inih-populate-prefix/src/inih-populate-stamp/inih-populate-mkdir"
+  "inih-populate-prefix/src/inih-populate-stamp/inih-populate-patch"
+  "inih-populate-prefix/src/inih-populate-stamp/inih-populate-test"
+  "inih-populate-prefix/src/inih-populate-stamp/inih-populate-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/inih-populate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
